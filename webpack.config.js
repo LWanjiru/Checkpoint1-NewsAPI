bbe5b30ce6a440867c5bae1f3382277ae68efea8
LWanjiru <linnette.wanjiru@andela.com>
2017-04-15 01:16:33 +0300

    "Change .gitignore and remove tab errors from webpack.config.js"

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 
 module.exports = {
-    entry: {
-        main: './src/scripts/main.js'
-    },
-    output: {
-        filename: './dist/scripts/[name].js'
-    },
-    devtool: 'source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel',
-                query: {
-                    presets: ['react', 'es2015']
-                }
-            }
-        ]
-    }
+  entry: {
+    main: './src/scripts/main.js',
+  },
+  output: {
+    filename: './dist/scripts/[name].js',
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015'],
+        },
+      },
+    ],
+  },
 };
